664ec760a93a6df2e7161f6066588bd8826f17d6
Andrei-WongE <awonge01@student.bbk.ac.uk>
2025-11-19 19:20:50 +0000

    Just started and already making mistakes...

--- Main.R
+++ Main.R
@@ -1,6 +1,10 @@
-# Using conda
-# conda create --name sedonapractise
-# conda activate sedonapractise
-# conda install -c conda-forge pyspark=3.5.3 apache-sedona=1.7.0 ipykernel pytest pip
-# pip install chispa, a testing library for PySpark DataFrames.
+# Using conda, but AVOID Conda dependency deadlock!!!
+# conda search r-base  --channel conda-forge
+# conda search rpy2  --channel conda-forge
 
+conda create -n sedonapractise r-base=4.4 r-essentials rpy2 python=3.11
+conda activate sedonapractise
+
+# - In RStudio:
+#   - Go to Tools > Global Options > General > R version
+# - Click Change and point it to the Conda R path (e.g., ~/miniconda3/envs/rgeoenv/bin/R)
