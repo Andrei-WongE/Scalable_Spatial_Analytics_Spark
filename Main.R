c77990b3e339658fc0bee98c033f8a0faad1049f
Andrei-WongE <awonge01@student.bbk.ac.uk>
2025-11-19 20:31:20 +0000

    I whish I know what I am doing...

--- Main.R
+++ Main.R
@@ -2,9 +2,28 @@
 # conda search r-base  --channel conda-forge
 # conda search rpy2  --channel conda-forge
 
-conda create -n sedonapractise r-base=4.4 r-essentials rpy2 python=3.11
-conda activate sedonapractise
+# rpy2                           3.6.4 py311r44h4625861_1  conda-forge
 
-# - In RStudio:
-#   - Go to Tools > Global Options > General > R version
-# - Click Change and point it to the Conda R path (e.g., ~/miniconda3/envs/rgeoenv/bin/R)
+# conda create -n sedonapractise -c conda-forge python=3.11 r-base=4.4 r-essentials rpy2=3.6.4
+# conda activate sedonapractise
+
+# Enable conda in terminal
+
+# # anaconda3\Scripts\conda.exe init cmd.exe
+# Restart Rstudio
+# conda activate sedonapractise
+
+# Pulling docket in terminal and binding ports
+
+# docker run -it apache/sedona:latest /bin/bash
+# docker run -d -e DRIVER_MEM=6g -e EXCECUTOR_MEN=8g -p 8888:8888 -p 8080:8080 -p 8081:8081 -p 4040:4040 -p 8085:8085
+
+# When using Docker with the WSL 2 backend, memory and CPU limits are managed by Windows,
+# not directly by Docker or the container. So setting DRIVER_MEM and EXECUTOR_MEM as environment variables
+# in docker run is only meaningful if the container or entrypoint script actually reads and applies them (e.g., to configure Spark).
+
+# apache/sedona : latest
+
+# docker run -it apache/sedona:latest /bin/bash
+# free -h
+# nproc
