546dbbc45a5edabbc36e0dcf121d410184469ba0
Andrei-WongE <awonge01@student.bbk.ac.uk>
2025-11-19 18:50:09 +0000

    Setting-up tha environment

---
+++ Main.R
@@ -0,0 +1,6 @@
+# Using conda
+# conda create --name sedonapractise
+# conda activate sedonapractise
+# conda install -c conda-forge pyspark=3.5.3 apache-sedona=1.7.0 ipykernel pytest pip
+# pip install chispa, a testing library for PySpark DataFrames.
+
